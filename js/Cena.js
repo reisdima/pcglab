54de2956aaf4a7c4aa68e24b58b98d83f0e4cd54
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2021-02-24 11:37:23 -0300

    Adiciona lista de sprites na cena

--- js/Cena.js
+++ js/Cena.js
@@ -1,12 +1,22 @@
 export default class Cena {
     // Esta classe é responsável por desenhar elementos na tela em uma animação
+
     constructor(canvas){
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
+        this.sprites = [];
     }
 
     desenhar(){
         this.ctx.fillStyle = "grey";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        for (let s = 0; s < this.sprites.length; s++) {
+            const sprite = this.sprites[s];
+            sprite.desenhar(this.ctx);
+        }
+    }
+
+    adicionar(sprite){
+        this.sprites.push(sprite);
     }
 }
--- js/Main.js
+++ js/Main.js
@@ -3,13 +3,13 @@ import Sprite from "./Sprite.js";
 console.log("Hello World!");
 
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
 console.log(canvas);
+
 const cena1 = new Cena(canvas);
+const pc = new Sprite();
+const en1 = new Sprite({x:140, w:30, color:"red"});
+
+cena1.adicionar(pc);
+cena1.adicionar(en1);
+
 cena1.desenhar();
-
-const pc = new Sprite({});
-const en1 = new Sprite({x:140, y:95, color:"red"});
-
-pc.desenhar(ctx);
-en1.desenhar(ctx);
