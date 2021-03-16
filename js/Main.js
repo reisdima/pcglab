7167406851398a4dab2f3179f7acee2fb17f54e9
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2021-03-16 12:02:55 -0300

    Cria gerenciador de entrada de controle

--- js/Main.js
+++ js/Main.js
@@ -5,8 +5,10 @@ import Mixer from "./Mixer.js";
 import Sprite from "./Sprite.js";
 import modeloMapa1 from "../maps/mapa1.js";
 import modeloMapa2 from "../maps/mapa2.js";
+import InputManager from "./InputManager.js";
 //console.log("Hello World!");
 
+const input = new InputManager();
 const mixer = new Mixer(10);
 const assets = new AssetManager(mixer);
 
@@ -20,30 +22,34 @@ assets.carregaAudio("boom", "assets/boom.wav");
 assets.carregaAudio("bruh", "assets/bruh.mp3");
 
 const canvas = document.querySelector("canvas");
-canvas.width = 20*32;
-canvas.height = 20*32;
-console.log(canvas);
+canvas.width = 14*32;
+canvas.height = 10*32;
+
+input.configurarTeclado({
+    "ArrowLeft": "MOVE-ESQUERDA",
+    "ArrowRight": "MOVE-DIREITA",
+});
 
 const cena1 = new Cena(canvas, assets);
 
-/*const mapa1 = new Mapa(10, 14, 32);
+const mapa1 = new Mapa(10, 14, 32);
 mapa1.carregaMapa(modeloMapa1);
-cena1.configuraMapa(mapa1);*/
+cena1.configuraMapa(mapa1);
 
-const mapa2 = new Mapa(10, 14, 32, cena1);
+/*const mapa2 = new Mapa(10, 14, 32, cena1);
 mapa2.carregaMapa(modeloMapa2);
-cena1.configuraMapa(mapa2);
+cena1.configuraMapa(mapa2);*/
 
-/*const pc = new Sprite({x:50, y :150, vx:20, h: 50});
-const en1 = new Sprite({x:160, vx: -10, color:"red"});
+const pc = new Sprite({x:50, y :150, vx:20, h: 20, w:20});
+const en1 = new Sprite({x:160, vx: -10, color:"red", h: 20, w:20});
 
 cena1.adicionar(pc);
 cena1.adicionar(en1);
-cena1.adicionar(new Sprite({x: 115, y:70, vy:10, color:"red"}));
-cena1.adicionar(new Sprite({x: 115, y:160, vy:-10, color:"red"}));*/
+cena1.adicionar(new Sprite({x: 115, y:70, vy:10, color:"red", h: 20, w:20}));
+cena1.adicionar(new Sprite({x: 115, y:160, vy:-10, color:"red", h: 20, w:20}));
 
 // Adiciona sprites mais fortes ("Protagonistas")
-cena1.adicionar(new Sprite({x: randValue(43, canvas.width - 43), y: randValue(43, canvas.height - 43), w:20, h:20,
+/*cena1.adicionar(new Sprite({x: randValue(43, canvas.width - 43), y: randValue(43, canvas.height - 43), w:20, h:20,
                             vy: randValue(-200, 200), vx: randValue(-200,200), color:"yellow", vida: 1000}));
 cena1.adicionar(new Sprite({x: randValue(43, canvas.width - 43), y: randValue(43, canvas.height - 43), w:20, h:20,
                             vy: randValue(-200, 200), vx: randValue(-200,200), color:"#BA55D3", vida: 1000}));
@@ -57,6 +63,7 @@ setInterval(() => {
     cena1.adicionar(new Sprite({x: randValue(43, canvas.width - 43), y: randValue(43, canvas.height - 43),
                                 vy: randValue(-100, 100), vx: randValue(-100,100), color:"red"}));
 }, 4000);
+*/
 
 cena1.iniciar();
 
