6b2d085ce580e5ba2b714b1e3bb723f1c6d032a2
Igor Knop <igorknop@gmail.com>
2021-04-07 01:28:18 -0300

    test: add constructor parameters

--- tests/Sprite.test.js
+++ tests/Sprite.test.js
@@ -1,15 +1,32 @@
-var Sprite = require("../classes/Sprite");
+const Sprite = require("../classes/Sprite");
 
 describe("Sprite class", () => {
   describe("is correctly created when", () => {
+
     it("is an object", () => {
-      var sprite = new Sprite();
+      const sprite = new Sprite();
       expect(typeof sprite).toBe("object");
     });
-    it("is has an initial position", () => {
-      var sprite = new Sprite();
+
+    it("has an initial position", () => {
+      const sprite = new Sprite();
+
       expect(sprite.x).toBeDefined();
       expect(sprite.y).toBeDefined();
+
+      const sprite2 = new Sprite({ x: 10, y: 20 });
+      expect(sprite2.x).toBe(10);
+      expect(sprite2.y).toBe(20);
+    });
+
+    it("has 2d velocity", () => {
+      const sprite = new Sprite();
+      expect(sprite.vx).toBeDefined();
+      expect(sprite.vy).toBeDefined();
+
+      const sprite2 = new Sprite({ vx: 10, vy: -10 });
+      expect(sprite2.vx).toBe(10);
+      expect(sprite2.vy).toBe(-10);
     });
   });
 });
