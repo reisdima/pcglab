97c0402d587fe50ead855f4c81a1174ca48dc56d
Winfer2 <rodridofire@hotmail.com>
2021-05-17 18:20:58 -0300

    Virou Wfc

--- Tcc/Tcc/Scripts/main.js
+++ Tcc/Tcc/Scripts/main.js
@@ -1,174 +1,224 @@
-// JavaScript source code
 
-class divisa {
-	constructor(biomeName, chance) {
-		this.biomeName = biomeName;
-		this.chance = chance;
-	}
+var tilesets = {
+labirinto: function(tool){
+  
+tool.addTile(`\
+@@@
+@@@
+@@@`,{weight:4})
+
+tool.addTile(`\
+@@@
+@d@
+@@@`)
+
+tool.addTile(`\
+...
+...
+...`)
+
+tool.addTile(`\
+..@
+..@
+..@`)
+
+tool.addTile(`\
+@@@
+..@
+..@`)
+  
+tool.addTile(`\
+@..
+...
+...`)
+
+tool.addColor("@", [244,164,96])
+tool.addColor(".", [139,69,19])
+tool.addColor("d", [0,255,0])
+  
+},
+
+
+salas: function(tool){
+  
+tool.addTile(`\
+@@@
+@@@
+@@@`,{weight:5})
+
+tool.addTile(`\
+...
+...
+...`)
+
+tool.addTile(`\
+@@@
+...
+...`)
+
+
+tool.addTile(`\
+..@
+...
+...`)
+
+
+
+tool.addColor("@", [255,255,255])
+tool.addColor(".", [0,0,0])
+  
+},
+
+}
+
+
+var worker;
+var canvas;
+var scene;
+var camera;
+var renderer;
+var root;
+
+function wfcDemo(tilesetName){
+  
+  if (worker){
+    worker.terminate();
+  }
+  
+  var tool = new ferramentaWfc();
+  
+  tilesets[tilesetName](tool);
+  
+  var viewport;
+  var wave;
+
+  var workerCode = function(){
+    var viewport;
+    var wfc;
+    var aspectRatio;
+    var size;
+    var increment;
+    var multiply;
+
+    console.log("connect")
+
+    onmessage = function(e) {
+      console.log(e)
+      if (e.data.op == "init"){
+        wfc = new WFC(e.data.wfcInput);
+        aspectRatio = e.data.aspectRatio;
+        size = e.data.initialSize;
+        increment = e.data.increment;
+        multiply = e.data.multiply
+        main();
+      }
+    }
+
+    function main(){
+      setTimeout(main,1);
+      if (!wfc){
+        return
+      }
+      if (wfc.step()){
+        viewport = {x:-size,y:-Math.round(size*aspectRatio),w:size*2,h:Math.round(size*2*aspectRatio)}
+        wfc.expand([viewport.y,viewport.x],[viewport.y+viewport.h,viewport.x+viewport.w]);
+        size=Math.ceil((size+increment)*multiply);
+      }
+      postMessage({viewport,wave:wfc.readout(/*false*/)})
+    }
+  }
+
+  console.log(tool.getTileFormulae())
+  
+  worker =new Worker(URL.createObjectURL(new Blob(["var WFC="+WFC.toString()+';('+workerCode.toString()+')()'])));// new Worker('worker.js');
+
+  worker.postMessage({
+    op:'init',
+    wfcInput:tool.generateWFCInput(),
+    aspectRatio:window.innerHeight/window.innerWidth,
+    initialSize:8,
+    increment:0,
+    multiply:1.5,
+  })
+
+  worker.onmessage = function(e){
+    viewport = e.data.viewport;
+    wave = e.data.wave;
+  }
+
+  if (renderer){
+    renderer.domElement.style.display="none";
+  }
+  
+  if (!canvas){
+    canvas = document.createElement("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.position = "absolute";
+    canvas.style.left = "0px";
+    canvas.style.top = "0px";
+    document.body.appendChild(canvas);
+  }else{
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+  }
+  
+  canvas.style.display="block";
+
+  function main(){
+    requestAnimationFrame(main)
+    // tool.clearPlotCache();
+    if (viewport && wave){
+      tool.plotWFCOutput(canvas,viewport,wave)
+    }
+  }
+  main();
 }
 
 
-class bioma {
-	constructor(name, power, divisas) {
-		this.name = name;
-		this.power = power;
-		this.divisas = divisas;
-	}
+var menubar = document.createElement("div");
+var title = document.createElement("span"); menubar.appendChild(title);
+title.innerHTML = "<b>NDWFC:</b> <i>Wave Function Collapse testing</i>"
+title.style.display="inline-block";
+title.style.marginTop="4px";
+menubar.style.zIndex="1000"
+menubar.style.position="absolute";
+menubar.style.left="0px";
+menubar.style.top="0px";
+menubar.style.background="rgba(0,0,0,0.6)"
+menubar.style.color="white";
+menubar.style.padding="10px"
+menubar.style.fontFamily="sans-serif";
+menubar.style.width=window.innerWidth+"px";
+menubar.style.fontSize="20px"
+
+var selbox = document.createElement("span"); menubar.appendChild(selbox);
+selbox.innerHTML = "Tileset = "
+selbox.style.marginRight="30px";
+selbox.style.float="right";
+// selbox.style.border="1px solid rgba(255,255,255,0.8)";
+selbox.style.padding="5px";
+selbox.style.fontSize="16px"
+
+var select = document.createElement("select");
+for (var k in tilesets){
+  var option = document.createElement("option");
+  option.value = k;
+  option.innerHTML = k;
+  select.appendChild(option);
 }
 
-class map {
-	newDoors(entrada){
-		if(this.saiu){
-			return;
-		}
-		var saida = Math.floor(Math.random() * 4)+1;
-		while(saida == entrada){
-			saida = Math.floor(Math.random() * 4)+1;
-		}
-		if(Math.random()<0.5) this.saiu = true;
-		this.setGates(saida);
-	}
-
-
-	setGates(entrada){
-		switch (entrada){
-			case 1:
-				this.entradaNorte = true;
-				this.newDoors(entrada);
-			break;
-			case 2:
-				this.entradaSul = true;
-				this.newDoors(entrada);
-			break;
-			case 3:
-				this.entradaLeste = true;
-				this.newDoors(entrada);
-			break;
-			case 4:
-				this.entradaOeste = true;
-				this.newDoors(entrada);
-			break;
-			case -1:
-				this.entradaNorte = true;
-				this.entradaSul = true;
-				this.entradaLeste = true;
-				this.entradaOeste = true;
-			break;
-		}
-	}
-
-	nextRoom(x,y,entrada){
-		var rng = Math.floor(Math.random() * 100)+1;
-		console.log(rng);
-		var acumulado = 0;
-		for(var i = 0; i<this.biome.divisas.length;i++){
-			if(rng<=acumulado+this.biome.divisas[i].chance){
-				grid[[x, y]] = new map(x,y, biomes[this.biome.divisas[i].biomeName], entrada);
-				posX = x;
-				posY = y;
-				updateScreen();
-				return;
-			}else{
-				acumulado+=this.biome.divisas[i].chance
-			}
-		}
-	
-	}
-
-	constructor(x, y, biome, entrada) {
-		this.altura = x;
-		this.largura = y;
-		this.biome = biome;
-		this.entradaNorte = false;
-		this.entradaSul = false;
-		this.entradaLeste = false;
-		this.entradaOeste = false;
-		this.setGates(entrada);
-		this.saiu = false;
-	}
+select.style.background="rgba(0,0,0,0)";
+select.style.color="white"
+select.style.fontSize="16px"
+select.onchange = function(){
+	wfcDemo(select.value);
 }
 
-
-function createGrassland(){
-	var divisas = [];
-	divisas.push(new divisa("Grassland", 10));
-	divisas.push(new divisa("Desert", 90));
-	return new bioma("Grassland", 100, divisas);
-}
-
-function CreateDesert(){
-	var divisas = [];
-	divisas.push(new divisa("Grassland", 90));
-	divisas.push(new divisa("Desert", 10));
-	return new bioma("Desert", 100, divisas);
-}
-
-var posX = 0;
-var posY = 0;
-grid = { };
-biomes = { };
-biomes["Grassland"] = createGrassland();
-biomes["Desert"] = CreateDesert();
+selbox.appendChild(select);
 
 
-grid[[0, 0]] = new map(0,0, biomes["Grassland"], 1);
+document.body.appendChild(menubar)
 
-function updateScreen(){
-	var mapa = grid[[posX, posY]]
-
-	$("#map_name").text("Bioma: " + mapa.biome.name + ". Coordenadas: " + posX + " , " + posY);
-
-	if(mapa.entradaNorte){
-		$("#norte").css("background-color", "lawngreen");
-		$("#norte").off().on( "click", function(e){
-			if(grid[[posX, posY+1]]){
-				posY++;
-				updateScreen()
-			}else mapa.nextRoom(posX,posY+1,2);
-		});
-	}else{
-		$("#norte").css("background-color", "orangered");
-		$("#norte").off().on( "click", false);
-	}
-
-	if(mapa.entradaSul){
-		$("#sul").css("background-color", "lawngreen");
-		$("#sul").off().on( "click", function(e){
-			if(grid[[posX, posY-1]]){
-				posY--;
-				updateScreen()
-			}else mapa.nextRoom(posX,posY-1,1);
-		});
-	}else{
-		$("#sul").css("background-color", "orangered");
-		$("#sul").off().on( "click", false);
-	}
-
-	if(mapa.entradaLeste){
-		$("#leste").css("background-color", "lawngreen");
-		$("#leste").off().on( "click", function(e){
-			if(grid[[posX+1, posY]]){
-				posX++;
-				updateScreen()
-			}else mapa.nextRoom(posX+1,posY,4);
-		});
-	}else{
-		$("#leste").css("background-color", "orangered");
-		$("#leste").off().on( "click", false);
-	}
-
-	if(mapa.entradaOeste){
-		$("#oeste").css("background-color", "lawngreen");
-		$("#oeste").off().on( "click", function(e){
-			if(grid[[posX-1, posY]]){
-				posX--;
-				updateScreen()
-			}else mapa.nextRoom(posX-1,posY,3);
-		});
-	}else{
-		$("#oeste").css("background-color", "orangered");
-		$("#oeste").off().on( "click", false);
-	}
-
-}
-updateScreen();
+select.value="labirinto"
+wfcDemo('labirinto');
