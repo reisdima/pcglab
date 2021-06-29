6f84cc7c30a1c1a303f5bd84b071f89f9c4fa6bd
Gabriel Martins <gabrielmartinssantana@gmail.com>
2021-06-29 14:35:37 -0300

    Adiciona teste do Sprite

---
+++ tests/Sprite2.test.js
@@ -0,0 +1,49 @@
+import Sprite from "../js/Sprite";
+
+describe("Classe Sprite", () => {
+  describe("Está corretamente criada quando:", () => {
+
+    it("É um objeto", () => {
+      const sprite = new Sprite();
+      expect(typeof sprite).toBe("object");
+    });
+
+    it("Possui tamanho válido", () => {
+      const sprite = new Sprite();
+      expect(sprite.h).toBeGreaterThan(0);
+      expect(sprite.w).toBeGreaterThan(0);
+    });
+
+    it("Tem posição inicial", () => {
+      const sprite = new Sprite();
+
+      expect(sprite.x).toBeDefined();
+      expect(sprite.y).toBeDefined();
+
+      const sprite2 = new Sprite({ x: 10, y: 20 });
+      expect(sprite2.x).toBe(10);
+      expect(sprite2.y).toBe(20);
+    });
+
+    it("Tem velocidade 2d", () => {
+      const sprite = new Sprite();
+      expect(sprite.vx).toBeDefined();
+      expect(sprite.vy).toBeDefined();
+
+      const sprite2 = new Sprite({ vx: 10, vy: -10 });
+      expect(sprite2.vx).toBe(10);
+      expect(sprite2.vy).toBe(-10);
+    });
+
+    it("Tem uma cor", () => {
+      const sprite = new Sprite();
+      expect(sprite.color).toBeDefined();
+    });
+
+    it("Tem mx e my = 0", () => {
+      const sprite = new Sprite();
+      expect(sprite.mx).toEqual(0);
+      expect(sprite.my).toEqual(0);
+    });
+  });
+});
