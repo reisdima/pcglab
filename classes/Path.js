e5aa517c3c8c072f7b7e58c670a4020815ed5aa8
Gabriel Martins <gabrielmartinssantana@gmail.com>
2021-07-08 14:44:29 -0300

    Corrige Path para export default class

--- classes/Path.js
+++ classes/Path.js
@@ -1,33 +1,35 @@
-function Path(linhas, colunas, size){
-    this.LINHAS = linhas;
-    this.COLUNAS = colunas;
-    this.SIZE = size;
-    this.steps = [];
-    for (let l = 0; l < this.linhas; l++) {
-        this.steps[l] = [];
-        for (let c = 0; c < this.colunas; c++) {
-            this.steps[l][c] = 0;
-        }
-    }
-}
+export default class Path{
+  constructor(linhas = 8, colunas = 12, tamanho = 32){
+      this.LINHAS = linhas;
+      this.COLUNAS = colunas;
+      this.SIZE = tamanho;
+      this.steps = [];
+      for (let l = 0; l < this.LINHAS; l++) {
+          this.steps[l] = [];
+          for (let c = 0; c < this.COLUNAS; c++) {
+              this.steps[l][c] = 0;
+          }
+      }
+  }
 
-Path.prototype.desenhar = function(ctx){
-    for (let l = 0; l < this.LINHAS; l++) {
-        for (let c = 0; c < this.COLUNAS; c++) {
-            ctx.font = "15px Arial";
-            ctx.fillStyle = "blue";
-            if(this.steps[l][c] === 1){
-                ctx.fillText(" O ", c*this.SIZE + this.SIZE/2, l*this.SIZE + this.SIZE/2 + 10);
-            }
-            ctx.strokeRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE); 
-        }
-    }
-}
+  desenhar(ctx){
+      for (let l = 0; l < this.LINHAS; l++) {
+          for (let c = 0; c < this.COLUNAS; c++) {
+              ctx.font = "15px Arial";
+              ctx.fillStyle = "blue";
+              if(this.steps[l][c] === 1){
+                  ctx.fillText(" O ", c*this.SIZE + this.SIZE/2, l*this.SIZE + this.SIZE/2 + 10);
+              }
+              ctx.strokeRect(c*this.SIZE, l*this.SIZE, this.SIZE, this.SIZE); 
+          }
+      }
+  }
 
-Path.prototype.addStep = function(x,y){
-    this.steps[x][y] = 1;
-}
+  addStep(x,y){
+      this.steps[x][y] = 1;
+  }
 
-Path.prototype.removeStep = function(x,y){
-    this.steps[x][y] = 1;
+  removeStep(x,y){
+      this.steps[x][y] = 0;
+  }
 }
