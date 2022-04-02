5f874aebcce10fd69d20581903c30e6913083470
Caio <caio.vincenzo@estudante.ufjf.br>
2022-04-02 19:56:07 -0300

    Passa a utilizar classe de js em todos arquivos

--- classes/Cell.js
+++ classes/Cell.js
@@ -1,45 +1,44 @@
-export default function Cell(params = {}) {
-    let exemplo = { 
-        tipo: 0, 
-        room: -3, 
-        distTeleportes: 999, 
-        distFirezones: 999,  
-        distInimigos: 999, 
-        distTesouros: 999, 
-        linha: 0, 
-        coluna: 0,
-        direcaoSaida: ">",
-        direcaoTesouros: "V",
-        notacaoTesouros: ".",
-        notacaoPlayer: ".   .",
-        distInundacaoSaida: -1,
-        distInundacaoTemp: -1,
-        indexRoom: -1,
-        vizinhos: []
-    };
- 
-    Object.assign(this, exemplo, params);   // Sobrescreve os atributos de params e exemplo na classe
-}
+export default class Cell {
+    constructor(params = {}) {
+        let exemplo = {
+            tipo: 0,
+            room: -3,
+            distTeleportes: 999,
+            distFirezones: 999,
+            distInimigos: 999,
+            distTesouros: 999,
+            linha: 0,
+            coluna: 0,
+            direcaoSaida: ">",
+            direcaoTesouros: "V",
+            notacaoTesouros: ".",
+            notacaoPlayer: ".   .",
+            distInundacaoSaida: -1,
+            distInundacaoTemp: -1,
+            indexRoom: -1,
+            vizinhos: []
+        };
 
-Cell.prototype = new Cell();
-Cell.prototype.constructor = Cell;
+        Object.assign(this, exemplo, params);   // Sobrescreve os atributos de params e exemplo na classe
+    }
 
-Cell.prototype.distInimigoTeleporte = function(maxInimigo = 1, maxTeleporte = 1){
-    return ((this.distInimigos * 0.5)/maxInimigo + (this.distTeleportes * 0.5)/maxTeleporte);
-}
+    distInimigoTeleporte(maxInimigo = 1, maxTeleporte = 1) {
+        return ((this.distInimigos * 0.5) / maxInimigo + (this.distTeleportes * 0.5) / maxTeleporte);
+    }
 
-Cell.prototype.distInimigo_Tesouro_Teleporte = function(maxInimigo = 1, maxTeleporte = 1, maxTesouro = 1){
-    return ((this.distInimigos * 0.5)/maxInimigo + (this.distTeleportes * 0.5)/maxTeleporte +
-    (this.distTesouros * 0.5)/maxTesouro);
-}
+    distInimigo_Tesouro_Teleporte(maxInimigo = 1, maxTeleporte = 1, maxTesouro = 1) {
+        return ((this.distInimigos * 0.5) / maxInimigo + (this.distTeleportes * 0.5) / maxTeleporte +
+            (this.distTesouros * 0.5) / maxTesouro);
+    }
 
-Cell.prototype.clone = function(celula){
-    this.tipo = celula.tipo; 
-    this.room = celula.room;
-    this.distTeleportes = celula.distTeleportes; 
-    this.distFirezones = celula.distFirezones;
-    this.distInimigos = celula.distInimigos;
-    this.distTesouros = celula.distTesouros;
-    this.linha = celula.linha;
-    this.coluna = celula.coluna;
+    clone(celula) {
+        this.tipo = celula.tipo;
+        this.room = celula.room;
+        this.distTeleportes = celula.distTeleportes;
+        this.distFirezones = celula.distFirezones;
+        this.distInimigos = celula.distInimigos;
+        this.distTesouros = celula.distTesouros;
+        this.linha = celula.linha;
+        this.coluna = celula.coluna;
+    }
 }
--- classes/Teleporter.js
+++ classes/Teleporter.js
@@ -1,145 +1,140 @@
 import Sprite from "./Sprite.js";
 import assetsMng from "./AssetsMng.js";
-import {setHud, getHud} from "./Hud.js";
+import { setHud, getHud } from "./Hud.js";
 
 const hud = getHud();
 
-export default function Teleporter(type){
+export default class Teleporter extends Sprite {
+
+  constructor(type) {
+    super({ s: 32, w: 32, h: 32 });
+
+    this.proximoTeleporte = undefined;
+    this.type = type;
+    this.roomNumber = -1;
+  }
+
   /**
-   * Estabelece a relação de Herança entre Player e Sprite:
-   *  -> Sprite é pai e player é filho
+   * GX => Coluna;
+   * GY => Linha
    */
-  Sprite.call(this, {s: 32, w: 32, h: 32});     
 
-  this.proximoTeleporte = undefined;
-  this.type = type;
-  this.roomNumber = -1;
-}
+  /**
+   * Retorna a referencia pra celula diretamente no mapa
+   */
 
-// Heranca
-Teleporter.prototype = new Sprite();              // Define que o Player é um Sprite
-Teleporter.prototype.constructor = Teleporter;
-
-/**
- * GX => Coluna;
- * GY => Linha
- */
-
-/**
- * Retorna a referencia pra celula diretamente no mapa
- */
-
-Teleporter.prototype.getCell = function(){
-  return this.map.cell[this.gy][this.gx];
-}
-
-Teleporter.prototype.setPosition = function(linha, coluna){
-   this.x = coluna * this.s + this.s/2;
-   this.y = linha * this.s  + this.s/2;
-}
-
-Teleporter.prototype.setPosition = function(celula){
-  this.x = celula.coluna * this.s + this.s/2;
-  this.y = celula.linha * this.s  + this.s/2;
-}
-
-Teleporter.prototype.copyTeleporte = function(teleporter, rooms){
-  //this.proximoTeleporte = teleporter.proximoTeleporte;
-  this.type = teleporter.type;
-  this.roomNumber = teleporter.roomNumber;
-  this.copy(teleporter);                              //Copia os dados do sprite
-}
-
-Teleporter.prototype.copyConnection = function(teleporter, rooms){
-  // Copiando o proximo teleporte
-  if(teleporter.proximoTeleporte != undefined){
-    let aux = rooms[teleporter.proximoTeleporte.roomNumber - 1];
-    if(this.type === 2){
-      this.proximoTeleporte = aux.teleporterFinal;
-    }
-    else{
-      this.proximoTeleporte = aux.teleporterInitial;
-    }
-    /*if(teleporter.proximoTeleporte.type === 2){     // inicio de sala
-      this.proximoTeleporte = aux.teleporterInitial;
-    }
-    else{                                           // Final de sala
-      this.proximoTeleporte = aux.teleporterFinal;
-    }*/
+  getCell() {
+    return this.map.cell[this.gy][this.gx];
   }
-}
 
-Teleporter.prototype.teleportar = function(player, levelAtual){
-  if(this.proximoTeleporte != undefined){
-    assetsMng.play("teleporte");
-    player.x = this.proximoTeleporte.x;
-    player.y = this.proximoTeleporte.y;
-    player.gx = this.proximoTeleporte.gx;
-    player.gy = this.proximoTeleporte.gy;
-    player.room = this.proximoTeleporte.roomNumber;
-    hud.bussola.update(levelAtual);
+  setPosition(linha, coluna) {
+    this.x = coluna * this.s + this.s / 2;
+    this.y = linha * this.s + this.s / 2;
   }
-  else{
-    console.log("proximoTeleporte eh undefined !!!");
-  }
-}
 
-Teleporter.prototype.desenhar = function(ctx){
-  switch(this.type){
-    case 0:                     // Início de fase
-      {
-        ctx.strokeStyle = "dark green";
-        ctx.fillStyle = "green";
-        ctx.linewidth = 10;
-        ctx.save();
-        ctx.globalAlpha = 0.40;         //Transparência
-        ctx.translate(this.x, this.y);
-        ctx.fillRect(-this.s/2, -this.s/2, this.s, this.s);
-        ctx.strokeRect(-this.s/2, -this.s/2, this.s, this.s);
-        ctx.restore();
+  setPosition(celula) {
+    this.x = celula.coluna * this.s + this.s / 2;
+    this.y = celula.linha * this.s + this.s / 2;
+  }
+
+  copyTeleporte(teleporter, rooms) {
+    //this.proximoTeleporte = teleporter.proximoTeleporte;
+    this.type = teleporter.type;
+    this.roomNumber = teleporter.roomNumber;
+    this.copy(teleporter);                              //Copia os dados do sprite
+  }
+
+  copyConnection(teleporter, rooms) {
+    // Copiando o proximo teleporte
+    if (teleporter.proximoTeleporte != undefined) {
+      let aux = rooms[teleporter.proximoTeleporte.roomNumber - 1];
+      if (this.type === 2) {
+        this.proximoTeleporte = aux.teleporterFinal;
       }
-      break;
-    case 1:                     // Final de fase
-      {
-        ctx.strokeStyle = "purple";
-        ctx.fillStyle = "rgb(84, 98, 139)";
-        ctx.linewidth = 10;
-        ctx.save();
-        ctx.globalAlpha = 0.60;         //Transparência
-        ctx.translate(this.x, this.y);
-        ctx.fillRect(-this.s/2, -this.s/2, this.s, this.s);
-        ctx.strokeRect(-this.s/2, -this.s/2, this.s, this.s);
-        ctx.restore(); 
+      else {
+        this.proximoTeleporte = aux.teleporterInitial;
       }
-      break;
-    case 2:                           // Teleporte Inicial room
-      {
-        ctx.strokeStyle = "darkblue";
-        ctx.fillStyle = "blue";
-        ctx.linewidth = 10;
-        ctx.save(); 
-        ctx.globalAlpha = 0.60;         //Transparência
-        ctx.translate(this.x, this.y);
-        ctx.fillRect(-this.s/2, -this.s/2, this.s, this.s);
-        ctx.strokeRect(-this.s/2, -this.s/2, this.s, this.s);
-        ctx.restore();
+      /*if(teleporter.proximoTeleporte.type === 2){     // inicio de sala
+        this.proximoTeleporte = aux.teleporterInitial;
       }
-      break;
-    case 3:                         // Teleporte final room
-      { 
-        ctx.strokeStyle = "Yellow";
-        ctx.fillStyle = "Orange";
-        ctx.linewidth = 100;
-        ctx.save();
-        ctx.globalAlpha = 0.40;         //Transparência
-        ctx.translate(this.x, this.y);
-        ctx.fillRect(-this.s/2, -this.s/2, this.s, this.s);
-        ctx.strokeRect(-this.s/2, -this.s/2, this.s, this.s);
-        ctx.restore();
-      }
-      break;
-    default:
-      console.log("Sprite type is wrong!!!");
-      break;
-  }                            
+      else{                                           // Final de sala
+        this.proximoTeleporte = aux.teleporterFinal;
+      }*/
+    }
+  }
+
+  teleportar(player, levelAtual) {
+    if (this.proximoTeleporte != undefined) {
+      assetsMng.play("teleporte");
+      player.x = this.proximoTeleporte.x;
+      player.y = this.proximoTeleporte.y;
+      player.gx = this.proximoTeleporte.gx;
+      player.gy = this.proximoTeleporte.gy;
+      player.room = this.proximoTeleporte.roomNumber;
+      hud.bussola.update(levelAtual);
+    }
+    else {
+      console.log("proximoTeleporte eh undefined !!!");
+    }
+  }
+
+  desenhar(ctx) {
+    switch (this.type) {
+      case 0:                     // Início de fase
+        {
+          ctx.strokeStyle = "dark green";
+          ctx.fillStyle = "green";
+          ctx.linewidth = 10;
+          ctx.save();
+          ctx.globalAlpha = 0.40;         //Transparência
+          ctx.translate(this.x, this.y);
+          ctx.fillRect(-this.s / 2, -this.s / 2, this.s, this.s);
+          ctx.strokeRect(-this.s / 2, -this.s / 2, this.s, this.s);
+          ctx.restore();
+        }
+        break;
+      case 1:                     // Final de fase
+        {
+          ctx.strokeStyle = "purple";
+          ctx.fillStyle = "rgb(84, 98, 139)";
+          ctx.linewidth = 10;
+          ctx.save();
+          ctx.globalAlpha = 0.60;         //Transparência
+          ctx.translate(this.x, this.y);
+          ctx.fillRect(-this.s / 2, -this.s / 2, this.s, this.s);
+          ctx.strokeRect(-this.s / 2, -this.s / 2, this.s, this.s);
+          ctx.restore();
+        }
+        break;
+      case 2:                           // Teleporte Inicial room
+        {
+          ctx.strokeStyle = "darkblue";
+          ctx.fillStyle = "blue";
+          ctx.linewidth = 10;
+          ctx.save();
+          ctx.globalAlpha = 0.60;         //Transparência
+          ctx.translate(this.x, this.y);
+          ctx.fillRect(-this.s / 2, -this.s / 2, this.s, this.s);
+          ctx.strokeRect(-this.s / 2, -this.s / 2, this.s, this.s);
+          ctx.restore();
+        }
+        break;
+      case 3:                         // Teleporte final room
+        {
+          ctx.strokeStyle = "Yellow";
+          ctx.fillStyle = "Orange";
+          ctx.linewidth = 100;
+          ctx.save();
+          ctx.globalAlpha = 0.40;         //Transparência
+          ctx.translate(this.x, this.y);
+          ctx.fillRect(-this.s / 2, -this.s / 2, this.s, this.s);
+          ctx.strokeRect(-this.s / 2, -this.s / 2, this.s, this.s);
+          ctx.restore();
+        }
+        break;
+      default:
+        console.log("Sprite type is wrong!!!");
+        break;
+    }
+  }
 }
