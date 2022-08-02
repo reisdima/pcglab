9de784d62714fc5a01228968284923351668342c
João Pereira <nashi.codes@gmail.com>
2022-08-02 16:05:37 -0300

    refatoração do codigo para o uso de classes

--- ecs/Entity.js
+++ ecs/Entity.js
@@ -1,34 +1,33 @@
-ECS.Entity = function Entity() {
-  this.id =
-    (+new Date()).toString(16) +
-    ((Math.random() * 100000000) | 0).toString(16) +
-    ECS.Entity._count;
+export default class Entity {
+  constructor(ECS) {
+    this.id =
+      (+new Date()).toString(16) +
+      ((Math.random() * 100000000) | 0).toString(16) +
+      ECS.entity_count;
 
-  ECS.Entity._count++;
+    ECS.entity_count++;
 
-  this.components = {};
-
-  return this;
-};
-
-ECS.Entity._count = 0;
-
-ECS.Entity.addComponet = function addComponent(component) {
-  this.component[component.name] = component;
-
-  return this;
-};
-
-ECS.Entity.removeComponent = function removeComponent(componentName) {
-  var name = componentName;
-  if (typeof componentName === "function") {
-    name = componentName.name;
+    this.components = {};
   }
-  delete this.component[name];
-  return this;
-};
 
-ECS.Entity.print = function print() {
-  console.log(JSON.stringify(this, null, 4));
-  return this;
+  addComponent(component) {
+    this.component[component.name] = component;
+
+    return this;
+  }
+
+  removeComponent(componentName) {
+    var name = componentName;
+    if (typeof componentName === "function") {
+      name = componentName.name;
+    }
+    delete this.component[name];
+    return this;
+  }
+  print() {
+    console.log(JSON.stringify(this, null, 4));
+    return this;
+}
+
+
 };
