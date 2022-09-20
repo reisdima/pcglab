5afb3947193994c85b0fbb391d8846b2ac4dacb1
João Pereira <nashi.codes@gmail.com>
2022-09-20 16:28:39 -0300

    sistema de movimento adicionaddo

--- ecs/systems/Render.js
+++ ecs/systems/Render.js
@@ -1,24 +1,15 @@
-export default function Render(entityManager, { ctx, canvas, t, t0, dt }) {
-  t0 = t0 ?? t;
-  dt = (t - t0) / 1000;
-
+export default function Render(entityManager, { ctx, canvas}) {
+  
   canvas.width = 960;
   canvas.height = 600;
   ctx.fillStyle = "darkslategray";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "lightgreen";
-
+  
   entityManager.entities.forEach((entity) => {
     const pos = entity.get("POSITION");
     const box = entity.get("BOX");
     ctx.fillRect(pos.x, pos.y, box.width, box.height);
   });
 
-  t0 = t;
-}
-
-requestAnimationFrame(desenha);
-
-function desenha(t) {
-  requestAnimationFrame(desenha);
 }
