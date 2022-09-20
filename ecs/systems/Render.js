bd6922bd70a3e5cd004a63754a249756f66a6755
João Pereira <nashi.codes@gmail.com>
2022-09-20 15:44:29 -0300

    variaveis necessarias adicionadas ao html

--- ecs/systems/Render.js
+++ ecs/systems/Render.js
@@ -1,32 +1,24 @@
-export default function Render(entityManager, {ctx, canvas}){
- ctx.fillStyle = "darkslategray";
- ctx.fillRect(0, 0, canvas.width, canvas.height);
- ctx.fillStyle = "lightgreen";
- entityManager.entities.forEach(entity => {
-  const pos = entity.get('POSITION');
-  const box = entity.get('BOX');
-   ctx.fillRect(pos.x, pos.y, box.width, box.height);
- });
+export default function Render(entityManager, { ctx, canvas, t, t0, dt }) {
+  t0 = t0 ?? t;
+  dt = (t - t0) / 1000;
 
+  canvas.width = 960;
+  canvas.height = 600;
+  ctx.fillStyle = "darkslategray";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "lightgreen";
+
+  entityManager.entities.forEach((entity) => {
+    const pos = entity.get("POSITION");
+    const box = entity.get("BOX");
+    ctx.fillRect(pos.x, pos.y, box.width, box.height);
+  });
+
+  t0 = t;
 }
 
+requestAnimationFrame(desenha);
 
-// const canvas = document.querySelector("canvas");
-// const ctx = canvas.getContext("2d");
-// canvas.width = 960;
-// canvas.height = 600;
-// let t0;
-// let dt;
-
-// requestAnimationFrame(desenha);
-
-// function desenha(t) {
-//   t0 = t0 ?? t;
-//   dt = (t - t0) / 1000;
-
-//   ctx.fillStyle = "darkslategray";
-//   ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-//   requestAnimationFrame(desenha);
-//   t0 = t;
-// }
+function desenha(t) {
+  requestAnimationFrame(desenha);
+}
