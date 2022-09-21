c42e2166406e6fa1558024378a66a0593045e205
João Pereira <nashi.codes@gmail.com>
2022-09-21 15:44:48 -0300

    function keyup working

--- ecs/systems/Controls.js
+++ ecs/systems/Controls.js
@@ -1,43 +1,47 @@
-export default function Controls(entityManager, { e }) {
+function keydown(entityManager, { e }) {
   entityManager.entities.forEach((entity) => {
     const v = entity.get("VELOCITY");
+    switch (e.key) {
+      case "w":
+        v.ay = -120;
+        break;
+      case "s":
+        v.ay = +120;
+        break;
+      case "a":
+        v.ax = -120;
+        break;
+      case "d":
+        v.ax = +120;
+        break;
 
-    if (e.type == "keydown") {
-      switch (e.key) {
-        case "w":
-          v.ay = -120;
-          break;
-        case "s":
-          v.ay = +120;
-          break;
-        case "a":
-          v.ax = -120;
-          break;
-        case "d":
-          v.ax = +120;
-          break;
-
-        default:
-          break;
-      }
-    } else if (e.type == "keyup") {
-      switch (e.key) {
-        case "w":
-          v.ay = 0;
-          break;
-        case "s":
-          v.ay = 0;
-          break;
-        case "a":
-          v.ax = 0;
-          break;
-        case "d":
-          v.ax = 0;
-          break;
-
-        default:
-          break;
-      }
+      default:
+        break;
     }
   });
 }
+
+function keyup(entityManager, { e }) {
+  entityManager.entities.forEach((entity) => {
+    const v = entity.get("VELOCITY");
+    switch (e.key) {
+      case "w":
+        v.ay = 0;
+        break;
+      case "s":
+        v.ay = 0;
+        break;
+      case "a":
+        v.ax = 0;
+        break;
+      case "d":
+        v.ax = 0;
+        break;
+
+      default:
+        break;
+    }
+  });
+}
+
+export { keydown, keyup };
