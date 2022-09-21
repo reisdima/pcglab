21866db641e75a3da9882e60d543b763c4a84d96
João Pereira <nashi.codes@gmail.com>
2022-09-20 23:06:23 -0300

    sistema de controle adicionado

--- ecs/systems/Move.js
+++ ecs/systems/Move.js
@@ -3,7 +3,9 @@ export default function Move(entityManager, { dt }) {
     const pos = entity.get("POSITION");
     const v = entity.get("VELOCITY");
     if (pos === undefined || v === undefined) return;
+    v.vx += v.ax * dt;
     pos.x += v.vx * dt;
+    v.vy += v.ay * dt;    
     pos.y += v.vy * dt;
   });
 }
